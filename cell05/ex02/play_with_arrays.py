29328dc43e47ce107d83156c10ed24757b3b8a44
bbimxn <67070102@kmitl.ac.th>
2026-02-11 21:30:52 +0700

    more cell05

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-array = [2,8,9,45,8,22,-12,2]
+array = [2,8,9,48,8,22,-12,2]
 print("Original array: ",array)
 Newarray = []
 for i in array:
-    Newarray.append(i+2)
+    if i > 5:
+        Newarray.append(i+2)
 
 print("New array: ",Newarray)        
